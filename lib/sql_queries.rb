f11525ed960888502f1f6bb55203621ec5cb3817
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-05-31 17:56:19 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,8 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   # find all pledges by project_id
   # SUM all pledges with same project_id
   # ORDER BY project title, ascending
-"SELECT Projects.title, SUM(Pledges.amount) FROM projects INNER JOIN pledges ON Pledges.project_id = Projects.id"
+"SELECT title, SUM(Pledges.amount) 
+  FROM projects INNER JOIN pledges ON Pledges.project_id = Projects.id"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
